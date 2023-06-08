bb37a5bd0a8cb69bc2909eb28632f009862adb6f
Wang Yi <1721436982@qq.com>
2023-06-08 22:03:55 +0800

    feat: added order table

--- src/pages/order/list/index.tsx
+++ src/pages/order/list/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { PageContainer } from '@ant-design/pro-components'
-
+import { PageContainer, ProTable } from '@ant-design/pro-components'
 
 type Props = {};
 
@@ -10,8 +9,19 @@ type Props = {};
  */
 const List: React.FC<Props> = () => {
   return (
-    <PageContainer>
-      Order List
+    <PageContainer title="订单列表">
+      <ProTable
+        columns={[
+          {
+            title: 'ID',
+            dataIndex: 'id'
+          },
+          {
+            title: '商品名称',
+            dataIndex: 'productName',
+          }
+        ]}
+      />
     </PageContainer>
   )
 }
