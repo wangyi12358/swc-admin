8e447eff9d1eb19294a8c3eefe2b271a1cb7ab26
Mika Wang <1721436982@qq.com>
2024-09-10 16:26:53 +0800

    delete note

--- src/pages/order/list/index.tsx
+++ src/pages/order/list/index.tsx
@@ -1,9 +1,5 @@
 import { PageContainer, ProTable } from '@ant-design/pro-components';
 
-/**
- * @Author: wangyi
- * @Date: 2023-06-06
- */
 function List() {
   return (
     <PageContainer title='订单列表'>
